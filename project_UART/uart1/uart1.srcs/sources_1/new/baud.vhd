8da244511e1e59e24453c0111883f6d901de9aa3
C-Seven-FL <hologramveo@gmail.com>
2023-04-15 14:29:19 +0200

    done

--- project_UART/uart1/uart1.srcs/sources_1/new/baud.vhd
+++ project_UART/uart1/uart1.srcs/sources_1/new/baud.vhd
@@ -1,49 +1,33 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 04/11/2023 01:33:11 PM
--- Design Name: 
--- Module Name: baud - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+-- This block uses code from clock_enable as a pulse generator for other blocks (RX/TX).
+-- It sends out a pulse every //baud_gen// clock pulses. 
+
+-- Example: baud_gen = 4800 ? p_clk_enable rises sig_cnt every cycle until sig_cnt >= baud_gen - 1
+-- Then it sends out a pulse that is one clock long and resets sig_cnt to 0. 
+-- That output pulse is fed into RX/TX blocks and tells those blocks to receive/transmit.
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use ieee.numeric_std.all; -- Package for arithmetic operations
 
 entity baud is
 
     Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
            baud_sw : in STD_LOGIC_VECTOR(2 downto 0);
-           baud_gen : out INTEGER
+           clk_baud : out STD_LOGIC --Output baud clock for RX/TX, using code from clock_enable
            );
 end baud;
 
 architecture behavioral of baud is
-   
+   --Local signal for storing baud rate
+  signal baud_gen : integer;
+   -- Local counter
+  signal sig_cnt : natural;
+  
 begin
 
-    baud_solve : process(clk) is
+    baud_solve : process(baud_sw) is
     begin
         case baud_sw is
             when "000" =>
@@ -62,7 +46,34 @@ begin
                 baud_gen <= 57600;
             when "111" =>
                 baud_gen <= 115200;
+            when others =>
+                baud_gen <= 4800;            
             end case;
      end process baud_solve;
         
+  --------------------------------------------------------
+  -- p_clk_enable:
+  -- Generate clock enable signal. By default, enable signal
+  -- is low and generated pulse is always one clock long.
+  --------------------------------------------------------
+  p_clk_enable : process (clk) is
+  begin
+
+    if rising_edge(clk) then              -- Synchronous process
+      if (rst = '1') then                 -- High-active reset
+        sig_cnt     <= 0;                 -- Clear local counter
+        clk_baud    <= '0';               -- Set output to low
+
+      -- Test number of clock periods
+      elsif (sig_cnt >= (baud_gen - 1)) then
+        sig_cnt <= 0;                     -- Clear local counter
+        clk_baud      <= '1';             -- Generate clock enable pulse
+      else
+        sig_cnt <= sig_cnt + 1;
+        clk_baud      <= '0';
+      end if;
+    end if;
+
+  end process p_clk_enable;
+  
 end Behavioral;
